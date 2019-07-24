620ba38c8424d3495cddb0e887b6c14370781de3
Phil Smith <pbsmithtbn@gmail.com>
2019-07-24 14:43:06 -0500

    Fill out the files to contain a final solution to the project

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import Display from './Display';
-import Controls from './Controls';
+import React, { useState } from "react";
+import Display from "./Display";
+import Controls from "./Controls";
 
 function Panel(props) {
+  const [count, setCount] = useState(0);
+
+  const changeCount = direction => {
+    setCount(count + direction);
+  };
+
   return (
     <div>
-      <Display />
-      <Controls />
-      <button className="btn-type">{props.type}</button>
+      <Display value={count} />
+      <Controls change={changeCount} />
+      <button onClick={() => props.math(count)} className="btn-type">
+        {props.type}
+      </button>
     </div>
-  )
+  );
 }
 
-export default Panel
+export default Panel;
